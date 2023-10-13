5608d1f3947b818432323e94bec0eabfa82fb50f
ruchitha <ruchitha33s>
2023-10-13 11:28:59 +0530

    my first commit

---
+++ sum.java
@@ -0,0 +1,16 @@
+public class sum
+{
+int x=20;
+int y=20;
+public void add()
+{
+int z=x+y;
+System.out.println("sum of "+x+" and"+y+"is"+z);
+}
+public static void main(String args[])
+{
+sum obj=new sum();
+obj.add();
+
+}
+}
